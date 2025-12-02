857422677019e96673cbc280e52398b415d23ed9
isaaclv822 <isaaclda183@gmail.com>
2025-12-02 12:33:46 +0100

    création du model article

--- project_cms/Models/Article.cs
+++ project_cms/Models/Article.cs
@@ -2,5 +2,10 @@
 {
     public class Article
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
